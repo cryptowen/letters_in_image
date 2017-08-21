8c0f7937670ca1b98168be79263ec8ef0978ece9
huwenchao <huwenchao@bytedance.com>
2017-08-21 22:00:57 +0800

    finish image to letters

--- util.py
+++ util.py
@@ -2,8 +2,10 @@
 from PIL import Image
 import pyocr
 import pyocr.builders
+from string import letters
 
 tool = pyocr.get_available_tools()[0]
+letters_set = set(letters)
 
 
 def image_to_string(image):
@@ -11,11 +13,18 @@ def image_to_string(image):
     return txt
 
 
+def letters_in_image(image):
+    txt = image_to_string(image)
+    res = sorted(set(txt) & letters_set)
+    return res
+
+
 def test_image_to_string_png():
     image = Image.open('test/test.png')
     txt = image_to_string(image)
     print(txt)
     assert txt
+
 
 def test_image_to_string_jpg():
     image = Image.open('test/test-european.jpg')
@@ -25,5 +34,10 @@ def test_image_to_string_jpg():
 
 
 if __name__ == "__main__":
-    test_image_to_string_png()
-    test_image_to_string_jpg()
+    # test_image_to_string_png()
+    # test_image_to_string_jpg()
+    image = Image.open('test/test-european.jpg')
+    txt = image_to_string(image)
+    print(txt)
+    letters = letters_in_image(image)
+    print(letters)
