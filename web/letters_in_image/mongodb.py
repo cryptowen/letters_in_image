504ef5c9d0d4e5cf2612ce1105dc6edb54f31eb5
huwenchao <huwenchao@bytedance.com>
2017-08-26 22:42:39 +0800

    add mongo timeout settings

--- web/letters_in_image/mongodb.py
+++ web/letters_in_image/mongodb.py
@@ -2,4 +2,10 @@
 from pymongo import MongoClient
 from django.conf import settings
 
-client = MongoClient(host=settings.MONGO_HOST, port=settings.MONGO_PORT)
+client = MongoClient(
+    host=settings.MONGO_HOST,
+    port=settings.MONGO_PORT,
+    socketTimeoutMS=1000,
+    connectTimeoutMS=100,
+    serverSelectionTimeoutMS=1000
+)
